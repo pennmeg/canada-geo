b582678360000117f35bd7208d98cc2039440b71
pennmeg <penn.meg@gmail.com>
2018-10-22 15:47:08 -0400

    minor edits to map

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 $(function(){
   console.log("-- Hello World --");
   $('#canada-map').vectorMap({
-    map: 'ca_lcc'
+    map: 'ca_lcc',
+    onRegionTipShow: function(e, el, code){
+      e.preventDefault();
+    }
   });
 });
