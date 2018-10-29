a31a9f6bfbd7728ef6f1894e8a5aa4654fa13959
Megan Penn <penn.meg@gmail.com>
2018-10-29 16:26:21 -0400

    prevent default form submit and set up keypress function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
 $(function(){
   console.log("-- Hello World --");
+  // Loading of Vector Map
   $('#canada-map').vectorMap({
     map: 'ca_lcc',
+    // Prevent the labels from showing for each province
     onRegionTipShow: function(e, el, code){
       e.preventDefault();
     }
+  });
+  // Prevent the form from submitting
+  $("form").submit(function(e){
+      e.preventDefault();
+  });
+  //
+  $('#answerBox').keypress(function(event){
+    // console.log("-- Key Press Loaded --");
+    console.log(String.fromCharCode(event.which));
+    console.log("Value: ", $('#answerBox').val());
   });
 });
