b1a1488824fc6821e8661be3c46ae3207c917d15
Megan Penn <penn.meg@gmail.com>
2018-10-29 17:00:02 -0400

    checkAnswer function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 $(function(){
   console.log("-- Hello World --");
-  // Loading of Vector Map
+  // ----- Loading of Vector Map
   $('#canada-map').vectorMap({
     map: 'ca_lcc',
     // Prevent the labels from showing for each province
@@ -8,14 +8,38 @@ $(function(){
       e.preventDefault();
     }
   });
-  // Prevent the form from submitting
+  // ----- List of provinces
+  var provinces = [
+    "British Columbia",
+    "Alberta",
+    "Manitoba",
+    "Saskatchewan",
+    "Ontario",
+    "Quebec",
+    "Newfoundland and Labrador",
+    "Nova Scotia",
+    "New Brunswick",
+    "Prince Edward Island",
+    "Yukon",
+    "North West Territories",
+    "Nunavut"
+  ];
+  // ----- Prevent the form from submitting / reloading
   $("form").submit(function(e){
       e.preventDefault();
   });
-  //
+  // ----- Checking the answer
   $('#answerBox').keypress(function(event){
-    // console.log("-- Key Press Loaded --");
-    console.log(String.fromCharCode(event.which));
-    console.log("Value: ", $('#answerBox').val());
+    // console.log(String.fromCharCode(event.which));
+    var checkAnswer = $('#answerBox').val();
+    // console.log("Value: ", $('#answerBox').val());
+    checkAnswer = checkAnswer.toLowerCase();
+    $.each(provinces,function(index,item){
+       provinces[index] = item.toLowerCase();
+       if (checkAnswer == item) {
+         console.log(item);
+         document.getElementById('answerBox').value='';
+       }
+    });
   });
 });
